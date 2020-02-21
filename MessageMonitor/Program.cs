147f572abc3ba56c97eb956f7d7a36a17fc6132b
Clarence Pollard <pollacm@gmail.com>
2020-02-20 22:05:30 -0500

    updates for message monitor

--- MessageMonitor/Program.cs
+++ MessageMonitor/Program.cs
@@ -11,11 +11,14 @@ using MessageMonitor;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.ObjectModel;
+using YoutubeSubscriberManager.Comment;
 
 namespace TRLWaiverMonitor
 {
     class Program
     {
+        public static List<Comment> commentsFromPage = new List<Comment>();
+
         static void Main(string[] args)
         {
             var acceptableWatchTimesForCalculation = new List<string>
@@ -23,6 +26,7 @@ namespace TRLWaiverMonitor
                 "minute",
                 "minutes"
             };
+            
             //SlackClientTest.TestPostMessage();
             //String pathToProfile = @"C:\Users\cxp6696\ChromeProfiles\User Data";
             String pathToProfile = @"C:\Users\Owner\ChromeProfiles\User Data";
@@ -78,12 +82,12 @@ namespace TRLWaiverMonitor
                 if (comment.FindElements(By.XPath("./ytcp-comment[@id='comment']//yt-formatted-string[@class='author-text style-scope ytcp-comment']")).Count == 1)
                 {
                     var commenterName = comment.FindElement(By.XPath("./ytcp-comment[@id='comment']//yt-formatted-string[@class='author-text style-scope ytcp-comment']")).Text;
-                    var message = new Message();
+                    var message = new Comment();
                     message.MessengerName = commenterName;
 
                     var videoName = comment.FindElement(By.XPath("./ytcp-comment[@id='comment']//div//ytcp-comment-video-thumbnail//a//yt-formatted-string")).Text;
                     message.VideoName = videoName;
-                    message.Comment = comment.FindElement(By.XPath("./ytcp-comment[@id='comment']//div//div[@id='content']//ytcp-comment-expander//div//yt-formatted-string")).Text;
+                    message.Message = comment.FindElement(By.XPath("./ytcp-comment[@id='comment']//div//div[@id='content']//ytcp-comment-expander//div//yt-formatted-string")).Text;
                     message.ListType = GetSubscriberType(commenterName);
 
                     var watchTimeAmount = comment.FindElement(By.XPath("./ytcp-comment[1]/div[1]/div[1]/div[2]/div[1]/yt-formatted-string[1]")).Text;
@@ -100,42 +104,60 @@ namespace TRLWaiverMonitor
 
                     var watchTimeInMinutes = int.Parse(watchTimeAmount.Split(' ')[0]);
 
+                    if (watchTimeInMinutes > 5)
+                        break;
+
                     var currentTime = DateTime.Now;
                     message.Time = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour, currentTime.Minute - watchTimeInMinutes, 0);
                     message.StartingTimeSlot = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour, 0, 0);
                     message.EndingTimeSlot = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour + 1, 0, 0);
 
+                    commentsFromPage.Add(message);
                 }
             }
+
+            var commentRepo = new CommentRepo();
+            var oldComments = commentRepo.GetEligableVideosUpdateOfMessengers();
+            foreach (var oldComment in oldComments)
+            {
+                commentRepo.SetMatchingCommenterNamesForTimeSlot(oldComments, oldComment);
+            }
+            foreach (var commentFromPage in commentsFromPage)
+            {
+                commentRepo.SetMatchingCommenterNamesForTimeSlot(commentsFromPage, commentFromPage);
+            }
+
+            commentsFromPage.AddRange(oldComments);
+            commentRepo.RefreshComments(commentsFromPage);
         }
 
-        private static Message.ListTypeEnum GetSubscriberType(string name)
+        private static Comment.ListTypeEnum GetSubscriberType(string name)
         {
             var partialName = name.Length > 12 ? name.Substring(0, 12).ToLower() : name.ToLower();
-            Message.ListTypeEnum messageType;
+            Comment.ListTypeEnum messageType;
             if (YoutubeSubscriberManager.Program.whitelist.Any(l => l.Contains(partialName)))
             {
-                messageType = Message.ListTypeEnum.White;
+                messageType = Comment.ListTypeEnum.White;
             }
             else if (YoutubeSubscriberManager.Program.orangelist.Any(l => l.Contains(partialName)))
             {
-                messageType = Message.ListTypeEnum.Orange;
+                messageType = Comment.ListTypeEnum.Orange;
             }
             else if (YoutubeSubscriberManager.Program.yellowlist.Any(l => l.Contains(partialName)))
             {
-                messageType = Message.ListTypeEnum.Yellow;
+                messageType = Comment.ListTypeEnum.Yellow;
             }
             else if (YoutubeSubscriberManager.Program.blacklist.Any(l => l.Contains(partialName)))
             {
-                messageType = Message.ListTypeEnum.Black;
+                messageType = Comment.ListTypeEnum.Black;
             }
             else if (YoutubeSubscriberManager.Program.pinklist.Any(l => l.Contains(partialName)))
             {
-                messageType = Message.ListTypeEnum.Pink;
+                messageType = Comment.ListTypeEnum.Pink;
             }
             else
             {
-                messageType = Message.ListTypeEnum.Other;
+                messageType = Comment.ListTypeEnum.Other;
             }
 
             return messageType;
